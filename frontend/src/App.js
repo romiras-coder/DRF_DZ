6bcda846533dde693f8f9291db8e13e034a508af
romiras <rom_iras@mail.ru>
2022-04-07 02:03:39 +0300

    DZ 2 ready

---
+++ frontend/src/App.js
@@ -0,0 +1,46 @@
+import React from 'react';
+import axios from 'axios'
+// import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css'
+import NavBar from './components/Navbar'
+import UsersList from './components/Users'
+
+class App extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      'users': []
+    }
+  }
+
+  componentDidMount() {
+    axios.get('http://127.0.0.1:8000/api/users/')
+      .then(response => {
+        const users = response.data
+        this.setState(
+          {
+            'users': users
+          }
+        )
+      }).catch(error => console.log(error))
+  }
+
+  render() {
+    return (
+      
+      <div>
+        <NavBar sticky="top"/>
+        <UsersList users={this.state.users} /> 
+      </div>
+    )
+  }
+}
+
+// function App() {
+//   return(
+//     <>
+//       <NavBar />
+//     </>
+//   )
+// }
+export default App;
--- todo/todo/settings.py
+++ todo/todo/settings.py
@@ -15,7 +15,6 @@ import os
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
@@ -27,7 +26,6 @@ DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
-
 # Application definition
 
 INSTALLED_APPS = [
@@ -37,6 +35,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'corsheaders',
     'rest_framework',
     'authapp'
 ]
@@ -46,6 +45,7 @@ AUTH_USER_MODEL = 'authapp.UserDRF'
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -73,7 +73,6 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'todo.wsgi.application'
 
-
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
@@ -83,7 +82,6 @@ DATABASES = {
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/3.0/ref/settings/#auth-password-validators
@@ -103,7 +101,6 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
 # Internationalization
 # https://docs.djangoproject.com/en/3.0/topics/i18n/
 
@@ -117,8 +114,12 @@ USE_L10N = True
 
 USE_TZ = True
 
-
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:3000",
+    "http://127.0.0.1:3000",
+]
