80634c102b42dc50e22aeb7b3e653da847fdd896
romiras <rom_iras@mail.ru>
2022-04-07 02:13:18 +0300

    DZ 2 ready

---
+++ frontend/src/components/Navbar.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import { Nav, Navbar, Container, NavDropdown, Form, FormControl, Button } from 'react-bootstrap';
+
+export default function NaviBar() {
+    return (
+        <Navbar bg="light" expand="lg">
+            <Container>
+                <Navbar.Brand href="#home">NaviBar</Navbar.Brand>
+                <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                <Navbar.Collapse id="basic-navbar-nav">
+                    <Nav className="me-auto">
+                        <Nav.Link href="#home">Home</Nav.Link>
+                        <Nav.Link href="#link">Link</Nav.Link>
+                        <NavDropdown title="Dropdown" id="basic-nav-dropdown">
+                            <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+                            <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
+                            <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+                            <NavDropdown.Divider />
+                            <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
+                        </NavDropdown>
+                    </Nav>
+                    <Form className="d-flex">
+                        <FormControl
+                            type="search"
+                            placeholder="Search"
+                            className="me-2"
+                            aria-label="Search"
+                        />
+                        <Button variant="outline-success">Search</Button>
+                    </Form>
+                </Navbar.Collapse>
+            </Container>
+        </Navbar>
+    )
+};
---
+++ frontend/src/components/Users.js
@@ -0,0 +1,46 @@
+import React from 'react'
+import { Table } from 'react-bootstrap';
+
+const UserItem = ({ user }) => {
+    return (
+        <tbody>
+            <tr>
+                <td>
+                    {user.username}
+                </td>
+                <td>
+                    {user.first_name}
+                </td>
+                <td>
+                    {user.last_name}
+                </td>
+                <td>
+                    {user.email}
+                </td>
+            </tr>
+        </tbody>
+    )
+}
+
+const UsersList = ({ users }) => {
+    return (
+        <Table striped bordered hover>
+            <thead>
+                <th>
+                    username
+                </th>
+                <th>
+                    first_name
+                </th>
+                <th>
+                    last_name
+                </th>
+                <th>
+                    email
+                </th>
+            </thead>
+            {users.map((user) => <UserItem user={user} />)}
+        </Table>
+    )
+}
+export default UsersList
